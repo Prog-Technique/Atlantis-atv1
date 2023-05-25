5c2aaef07fb7c690ed9404daa1a9eec4b902d1ec
amandavo <amandavoliveira0@gmail.com>
2023-05-25 20:10:09 -0300

    trocando telefone de dependente para clone

--- atlantis/teste/index.ts
+++ atlantis/teste/index.ts
@@ -49,19 +49,11 @@ dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 
-let dependenteTelefone1 = new Telefone()
-dependenteTelefone1.ddd = "12"
-dependenteTelefone1.numero = "993912428"
-dependente.telefones.push(dependenteTelefone1)
-
-let dependenteTelefone2 = new Telefone()
-dependenteTelefone2.ddd = "12"
-dependenteTelefone2.numero = "994924246"
-dependente.telefones.push(dependenteTelefone2)
-
-// ---------------------------------------
+dependente.telefones = ( cliente.telefones.map(i => i.clonar()) as Telefone[])
 
 dependente.endereco = cliente.endereco.clonar() as Endereco
+
+// ---------------------------------------
 
 let dependenteDocumento1 = new Documento()
 dependenteDocumento1.tipo = TipoDocumento.CPF
@@ -82,57 +74,5 @@ dependente.documentos = cliente.documentos
 dependente.titular = cliente
 cliente.dependentes
 
-// console.log(cliente,'\n');
-// console.log(dependente);
-
-console.log("Cliente:");
-console.log("Nome:", cliente.nome);
-console.log("Nome Social:", cliente.nomeSocial);
-console.log("Data de Cadastro:", cliente.dataCadastro.toLocaleString());
-console.log("Data de Nascimento:", cliente.dataNascimento.toLocaleString());
-console.log("Telefones:");
-cliente.telefones.forEach((telefone, index) => {
-  console.log("Telefone", index + 1);
-  console.log("DDD:", telefone.ddd);
-  console.log("Número:", telefone.numero);
-});
-console.log("Endereço:");
-console.log("Rua:", cliente.endereco.rua);
-console.log("Bairro:", cliente.endereco.bairro);
-console.log("Cidade:", cliente.endereco.cidade);
-console.log("Estado:", cliente.endereco.estado);
-console.log("País:", cliente.endereco.pais);
-console.log("Código Postal:", cliente.endereco.codigoPostal);
-console.log("Documentos:");
-cliente.documentos.forEach((documento, index) => {
-  console.log("Documento", index + 1);
-  console.log("Tipo:", documento.tipo);
-  console.log("Número:", documento.numero);
-  console.log("Data de Expedição:", documento.dataExpedicao.toLocaleString());
-});
-
-console.log("\nDependente:");
-console.log("Nome:", dependente.nome);
-console.log("Nome Social:", dependente.nomeSocial);
-console.log("Data de Cadastro:", dependente.dataCadastro.toLocaleString());
-console.log("Data de Nascimento:", dependente.dataNascimento.toLocaleString());
-console.log("Telefones:");
-dependente.telefones.forEach((telefone, index) => {
-  console.log("Telefone", index + 1);
-  console.log("DDD:", telefone.ddd);
-  console.log("Número:", telefone.numero);
-});
-console.log("Endereço:");
-console.log("Rua:", dependente.endereco.rua);
-console.log("Bairro:", dependente.endereco.bairro);
-console.log("Cidade:", dependente.endereco.cidade);
-console.log("Estado:", dependente.endereco.estado);
-console.log("País:", dependente.endereco.pais);
-console.log("Código Postal:", dependente.endereco.codigoPostal);
-console.log("Documentos:");
-dependente.documentos.forEach((documento, index) => {
-  console.log("Documento", index + 1);
-  console.log("Tipo:", documento.tipo);
-  console.log("Número:", documento.numero);
-  console.log("Data de Expedição:", documento.dataExpedicao.toLocaleString());
-});
+console.log(cliente,'\n');
+console.log(dependente);
